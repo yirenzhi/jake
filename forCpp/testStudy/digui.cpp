a876e16ad4fc170e05ebfefbf95a41b6aada40f4
yirenzhi <yiren_zhi@163.com>
2016-09-09 18:07:12 +0800

    增加递归测试

--- forCpp/testStudy/digui.cpp
+++ forCpp/testStudy/digui.cpp
@@ -31,3 +31,108 @@ void hannoi(int n,char from,char buffer,char to)
 		hannoi (n-1, buffer, from, to);
 	}
 }
+
+
+void heneita(int n ,char from,char buffer,char to)
+{
+	if (1==n)
+	{
+		cout<<"Move disk"<<n<<" from "<<from<<" to "<<to<<endl;
+	}
+	else
+	{
+		heneita(n-1,from,to,buffer);
+		cout<<"Move disk"<<n<<" from "<<from<<" to "<<to<<endl;
+		heneita(n-1,buffer,from,to);
+	}
+}
+
+int feibona(int n)
+{
+	if (1==n)
+	{
+		return 1;
+	}
+	else
+	{
+		return feibona(n-1)+feibona(n-2);
+	}
+}
+
+
+//链表递归
+
+typedef struct node
+{
+	int data;
+	struct node* next;
+}*linkList;
+
+ node* initlinkList(int num[],int length)
+{
+	if (length<1)
+	{
+		return NULL;
+	}
+
+	node* headnode = (node*) malloc(sizeof(node));
+	headnode->data=num[0];
+	headnode->next=NULL;
+	if (length>1)
+	{
+		node* p = headnode;
+		for (int i = 1; i < length; i++)
+		{
+			node* newnode = (node*) malloc(sizeof(node));
+			newnode->data=num[i];
+			newnode->next=NULL;
+			p->next=newnode;
+			p=newnode;
+		}
+	}
+	return headnode;
+}
+
+ void printLink(linkList head)
+ {
+	 while (head!=NULL)
+	 {
+		 cout<<head->data<<endl;
+		 head=head->next;
+	 }
+ }
+
+ //递归求链表长度
+ int getlengthRecursion(linkList head)
+ {
+	 if (head!=NULL)
+	 {
+		 return getlengthRecursion(head->next)+1;
+	 }
+	 else
+	 {
+		 return 0;
+	 }
+ }
+
+ //尾递归求链表长度
+ int getlengthTailRecursion(linkList head,int *num)
+ {
+	 if (NULL==head)
+	 {
+		 return *num;
+	 }
+	 else
+	 {
+		 *num = *num + 1;
+		 return getlengthTailRecursion(head->next,num);
+	 }
+
+ }
+
+
+ //test std::for_each
+ void stdForeach()
+ {
+
+ }
--- forCpp/testStudy/test1.cpp
+++ forCpp/testStudy/test1.cpp
@@ -51,6 +51,25 @@
  }
 
 extern void hannoi(int n,char from,char buffer,char to);
+extern void heneita(int n ,char from,char buffer,char to);
+extern int feibona(int n);
+
+typedef struct node *linkList;
+extern  node* initlinkList(int num[],int length);
+extern void printLink(linkList head);
+
+extern int getlengthRecursion(linkList head);
+
+extern int getlengthTailRecursion(linkList head,int *num);
+
+extern void stdForeach();
+
+struct fle
+{
+	char val;
+	int array[];
+};
+
 int main()
 {
 // 	string get = reverseString("hello");
@@ -62,12 +81,39 @@ int main()
 	//测试线程
 //	testThread::runThread3();
 
-	cout<<jiecheng(5)<<endl;
+	//cout<<jiecheng(5)<<endl;
 
-	cout<<"请输入汉诺塔的层数:";
-	int a;
-	cin>>a;
-	hannoi(a,'A','B','C');
+	//cout<<"请输入汉诺塔的层数:";
+	//int a;
+	//cin>>a;
+	//hannoi(a,'A','B','C');
+	//heneita(a,'A','B','C');
+
+	//feibona(5);
+
+	//测试链表
+	//int nums[]={0,1,2,3,4};
+	//cout<<"数组长度为："<<sizeof(nums)/sizeof(int)<<endl;
+	//linkList list1= initlinkList(nums,sizeof(nums)/sizeof(int));
+	//printLink(list1);
+
+	//int len =0;
+	//cout<<"递归法求链表长度为:"<<getlengthRecursion(list1)<<endl;
+
+	//cout<<"尾递归求链表长度为:"<<getlengthTailRecursion(list1,&len)<<endl;
+
+	//string 的长度为28
+	//string menus[] = {"new","start","regret"};
+	//cout<<"长度"<<sizeof(menus)<<endl;
+	//cout<<"长度"<<sizeof(menus[0])<<endl;
+	//string str = "012345678901234567890123456789012345678901234567890123456789";
+	//string *p=&str;
+	//cout<<"长度"<<sizeof(str)<<endl;
+	//cout<<"长度"<<strlen(str.c_str())<<endl;
+	//cout<<"长度"<<sizeof(string)<<endl;
+	//cout<<"长度"<<sizeof(&str)<<endl;
+	//cout<<"长度"<<sizeof(struct fle)<<endl;
+	
 	while(true){
 
 	}
